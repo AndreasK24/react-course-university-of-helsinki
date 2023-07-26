948e5b2bd59db2b3b9eaea9a1e7f9344216692af
@AndreasK24 <@Andreas24>
2023-07-26 16:03:04 -0300

    Exercises 3.15 - 3.18

--- part3/index.js
+++ part3/index.js
@@ -5,6 +5,17 @@ const cors = require("cors");
 
 const app = express();
 const Person = require("./models/person");
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("build"));
@@ -16,6 +27,7 @@ app.use(
 morgan.token("content", (req, res) =>
   req.method === "POST" ? JSON.stringify(req.body) : "-"
 );
+
 /*const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
   console.log("Path:  ", request.path);
@@ -97,22 +109,44 @@ app.post("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
-  /*if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
+  /*
   console.log(person);
   response.json(person);*/
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  Person.deleteById(request.params.id).then((person) => {
-    response.json(person);
-  });
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -120,6 +154,9 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+// this has to be the last loaded middleware.
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
